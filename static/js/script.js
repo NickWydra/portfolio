2e1adc838c9a146eb02a458ed5e1a46e097d09a1
NickWydra <njwydra@umich.edu>
2017-06-06 01:19:50 -0400

    massice removal of old, start of new

---
+++ static/js/script.js
@@ -0,0 +1,33 @@
+var current_frame, total_frames, path, length, handle, myobj;
+
+myobj = document.getElementById('NWBar').cloneNode(true);
+
+var init = function() {
+  current_frame = 0;
+  total_frames = 150;
+  path = new Array();
+  length = new Array();
+  i = 0;
+  path[i] = document.getElementById('i'+i);
+  l = path[i].getTotalLength();
+  length[i] = l;
+  path[i].style.strokeDasharray = l + ' ' + l; 
+  path[i].style.strokeDashoffset = l;
+  handle = 0;
+}
+
+var draw = function() {
+   var progress = current_frame/total_frames;
+   if (progress > 1) {
+     window.cancelAnimationFrame(handle);
+   } else {
+     current_frame++;
+     for(var j=0; j<path.length;j++){
+         path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
+     }
+     handle = window.requestAnimationFrame(draw);
+   }
+};
+
+init();
+draw();
